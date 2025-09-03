c049eb4c840d6a20088f5494bc12b655c5c39ab9
J <j@sin.io>
2025-09-03 19:09:01 +0000

    NO .DS_Store

--- OSX/.config/fish/config.fish
+++ OSX/.config/fish/config.fish
@@ -1,94 +1,97 @@
 if status is-interactive
 
-#set -gz ANTHROPIC_BASE_URL http://localhost:8080
-set -gx SSH_AUTH_SOCK /Users/j/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock
-set -gx COLORTERM truecolor
-set -gx EDITOR emacsclient
-set -gx PAGER nvimpager
-set -gx LANG en_US.UTF-8    # Adjust this to your language!
-set -gx LC_ALL en_US.UTF-8  # Adjust this to your locale!
-#set -gx VIRTUAL_ENV_DISABLE_PROMPT true
-set -gx GOPATH $HOME/go
-set -x PATH $GOPATH/bin $HOME/.composer/vendor/bin $HOME/Library/Python/3.7/bin $PATH
-set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
-set -gx DOCKER_BUILDKIT 1
-set -gx COMPOSE_DOCKER_CLI_BUILD 1
-#set -g fish_key_bindings fish_vi_key_bindings
-#set -g fish_bind_mode insert
-fish_default_key_bindings
+    #set -gz ANTHROPIC_BASE_URL http://localhost:8080
 
-# Title options - Modified for tmux window naming
-set -g theme_title_display_process yes
-set -g theme_title_display_path no
-set -g theme_title_display_user no
-set -g theme_title_use_abbreviated_path no
+    set -gx SSH_AUTH_SOCK /Users/j/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock
+    set -gx COLORTERM truecolor
+    set -gx EDITOR emacsclient
+    set -gx PAGER nvimpager
+    set -gx LANG en_US.UTF-8 # Adjust this to your language!
+    set -gx LC_ALL en_US.UTF-8 # Adjust this to your locale!
+    #set -gx VIRTUAL_ENV_DISABLE_PROMPT true
+    set -gx GOPATH $HOME/go
+    set -x PATH $GOPATH/bin $HOME/.composer/vendor/bin $HOME/Library/Python/3.7/bin $PATH
+    set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
+    set -gx DOCKER_BUILDKIT 1
+    set -gx COMPOSE_DOCKER_CLI_BUILD 1
+    #set -g fish_key_bindings fish_vi_key_bindings
+    #set -g fish_bind_mode insert
+    fish_default_key_bindings
 
-# Prompt options
-set -g theme_display_ruby no
-set -g theme_display_virtualenv no
-set -g theme_display_vagrant no
-set -g theme_display_vi no
-set -g theme_display_k8s_context no # yes
-set -g theme_display_user yes
-set -g theme_display_hostname yes
-set -g theme_show_exit_status yes
-set -g theme_git_worktree_support no
-set -g theme_display_git yes
-set -g theme_display_git_dirty yes
-set -g theme_display_git_untracked yes
-set -g theme_display_git_ahead_verbose yes
-set -g theme_display_git_dirty_verbose yes
-set -g theme_display_git_master_branch yes
-set -g theme_display_date yes
-set -g theme_display_cmd_duration yes
-set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts yes
-set -g theme_color_scheme solarized-dark
+    # Title options - Modified for tmux window naming
+    set -g theme_title_display_process yes
+    set -g theme_title_display_path no
+    set -g theme_title_display_user no
+    set -g theme_title_use_abbreviated_path no
 
-bind -M insert \cg forget
+    # Prompt options
+    set -g theme_display_ruby no
+    set -g theme_display_virtualenv no
+    set -g theme_display_vagrant no
+    set -g theme_display_vi no
+    set -g theme_display_k8s_context no # yes
+    set -g theme_display_user yes
+    set -g theme_display_hostname yes
+    set -g theme_show_exit_status yes
+    set -g theme_git_worktree_support no
+    set -g theme_display_git yes
+    set -g theme_display_git_dirty yes
+    set -g theme_display_git_untracked yes
+    set -g theme_display_git_ahead_verbose yes
+    set -g theme_display_git_dirty_verbose yes
+    set -g theme_display_git_master_branch yes
+    set -g theme_display_date yes
+    set -g theme_display_cmd_duration yes
+    set -g theme_powerline_fonts yes
+    set -g theme_nerd_fonts yes
+    set -g theme_color_scheme solarized-dark
 
+    bind -M insert \cg forget
 
-# TMUX
-set fish_tmux_autostart true
-set fish_tmux_autostart_once false
-set fish_tmux_autoconnect false
-set fish_tmux_autoquit true
+    # TMUX
+    set fish_tmux_autostart true
+    set fish_tmux_autostart_once false
+    set fish_tmux_autoconnect false
+    set fish_tmux_autoquit true
 
+    # `ls` → `ls -laG` abbreviation
+    abbr -a -g ls ls -laG
 
-# `ls` → `ls -laG` abbreviation
-abbr -a -g ls ls -laG
- 
-# `ls` → `eza` abbreviation
-# Requires `brew install exa`
-if type -q eza
-  abbr --add -g ls 'eza --long --classify --all --git --tree --level 1'
-end
- 
-# `cat` → `bat` abbreviation
-# Requires `brew install nvimpager'
-if type -q nvimpager
-  abbr --add -g cat 'nvimpager'
-end
+    # `ls` → `eza` abbreviation
+    # Requires `brew install exa`
+    if type -q eza
+        abbr --add -g ls 'eza --long --classify --all --git --tree --level 1'
+    end
 
-# `zoxide` → `z` abbreviation
-# Requires `brew install zoxide`
-if type -q bat
-  abbr --add -g z 'zoxide'
-end
+    # `cat` → `bat` abbreviation
+    # Requires `brew install nvimpager'
+    if type -q nvimpager
+        abbr --add -g cat nvimpager
+    end
 
-# `vi` → `nvim` abbreviation
-# Requires `brew install zoxide`
-if type -q nvim
-  abbr --add -g vi 'nvim'
-end
+    # `zoxide` → `z` abbreviation
+    # Requires `brew install zoxide
+    if type -q bat
+        abbr --add -g z zoxide
+    end
 
-# `e` → `emacsclient` abbreviation
-if type -q emacsclient
-  abbr --add -g e 'emacsclient'
-end
+    # `vi` → `nvim` abbreviation
+    # Requires `brew install neovim
+    if type -q nvim
+        abbr --add -g vi nvim
+    end
 
+    # `e` → `emacsclient` abbreviation
+    if type -q emacsclient
+        abbr --add -g e emacsclient
+    end
 
+    # `tar` → `gtar` abbreviation
+    # Requires `brew install gtar
+    if type -q gtar
+        abbr --add -g tar gtar
+    end
 
-nvm use latest
- 
+    nvm use latest
+
 end
